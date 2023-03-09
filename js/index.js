90ebcb0ea33395f3b7a982955c49e13c99c7b01c
mhull99 <112908319+mhull99@users.noreply.github.com>
2023-03-09 09:57:58 -0500

    images

--- js/index.js
+++ js/index.js
@@ -1,2 +1,13 @@
 
 console.log("Your index.js file is loaded correctly!");
+
+$(document).ready(function() {
+    $('.tabContainer:first').show();
+    $('.tabNav:first').addClass('.active');
+
+    $('.tabNav:first').click(function(event){
+        index= $(this).index();
+        $('.tabNav li').removeClass('active');
+        $(this).addClass('active');
+    });
+});
