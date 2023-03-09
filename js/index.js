743dcaedc63ffb5c6f68c9a544cbc10998ddbff9
mhull99 <112908319+mhull99@users.noreply.github.com>
2023-03-09 10:29:21 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,14 +1,15 @@
 console.log("Your index.js file is loaded correctly!");
 
 $(document).ready(function(){
-    $('.tabContainer:first').show();
-    $('.tabNav:first').addClass('.active');
+    $(".tabContainer:first").show();
+    $(".tabNav:first").addClass(".active");
 
-    $('.tabNav:first').click(function(event){
+    $(".tabNav:first").click(function(event){
         index= $(this).index();
-        $('.tabNav li').removeClass('active');
+        $(".tabNav li").removeClass('active');
         $(this).addClass('active');
         $('.tabContainer').hide();
         $('.tabContainer').eq(index).show();
     });
 });
+
